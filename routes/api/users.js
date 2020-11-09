ff884b39f9a02ead956817117288ab47e52cc75e
dbacall <dbacall@hotmail.co.uk>
2020-11-09 15:34:38 +0000

    test: add test for if the email and password field are left empty when logging in

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,18 +1,18 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const keys = require("../../config/keys");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 // Load input validation
-const validateRegisterInput = require("../../validation/register");
-const validateLoginInput = require("../../validation/login");
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 // Load User model
-const User = require("../../models/User");
+const User = require('../../models/User');
 
 // @route POST api/users/register
 // @desc Register user
 // @access Public
-router.post("/register", (req, res) => {
+router.post('/register', (req, res) => {
   // Form validation
   const { errors, isValid } = validateRegisterInput(req.body);
   // Check validation
@@ -21,7 +21,7 @@ router.post("/register", (req, res) => {
   }
   User.findOne({ email: req.body.email }).then((user) => {
     if (user) {
-      return res.status(400).json({ email: "Email already exists" });
+      return res.status(400).json({ email: 'Email already exists' });
     } else {
       const newUser = new User({
         firstName: req.body.firstName,
@@ -47,7 +47,7 @@ router.post("/register", (req, res) => {
 // @route POST api/users/login
 // @desc Login user and return JWT token
 // @access Public
-router.post("/login", (req, res) => {
+router.post('/login', (req, res) => {
   // Form validation
   const { errors, isValid } = validateLoginInput(req.body);
   // Check validation
@@ -60,7 +60,7 @@ router.post("/login", (req, res) => {
   User.findOne({ email }).then((user) => {
     // Check if user exists
     if (!user) {
-      return res.status(404).json({ emailnotfound: "Email not found" });
+      return res.status(404).json({ emailnotfound: 'Email not found' });
     }
     // Check password
     bcrypt.compare(password, user.password).then((isMatch) => {
@@ -83,14 +83,14 @@ router.post("/login", (req, res) => {
           (err, token) => {
             res.json({
               success: true,
-              token: "Bearer " + token,
+              token: 'Bearer ' + token,
             });
           }
         );
       } else {
         return res
           .status(400)
-          .json({ passwordincorrect: "Password incorrect" });
+          .json({ passwordIncorrect: 'Password incorrect' });
       }
     });
   });
--- test/login.test.js
+++ test/login.test.js
@@ -29,4 +29,33 @@ describe('Login', () => {
 
     expect(data.body.emailnotfound).to.equal('Email not found');
   });
+
+  it('should return an error if user inputs wrong password', async () => {
+    await registerHelpers.registerUser(
+      'David',
+      'Bacall',
+      'dbacall@hotmail.co.uk',
+      'password',
+      'password'
+    );
+
+    var data = await loginHelpers.loginUser('dbacall@hotmail.co.uk', 'passwo');
+
+    expect(data.body.passwordIncorrect).to.equal('Password incorrect');
+  });
+
+  it('should return errors if email and password field are left empty', async () => {
+    await registerHelpers.registerUser(
+      'David',
+      'Bacall',
+      'dbacall@hotmail.co.uk',
+      'password',
+      'password'
+    );
+
+    var data = await loginHelpers.loginUser('', '');
+
+    expect(data.body.email).to.equal('Email field is required');
+    expect(data.body.password).to.equal('Password field is required');
+  });
 });
