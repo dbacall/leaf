4453a42ae27edc94857e7ba86745dfb4606697c3
dbacall <dbacall@hotmail.co.uk>
2020-11-10 15:35:32 +0000

    refactor: split up home component using container

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -2,12 +2,10 @@ import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 const Home = (props) => {
-  const name = useSelector((state) => state.auth.user.name);
-
   return (
     <div>
       <h1>Home</h1>
-      <h2>Hello {name}</h2>
+      <h2>Hello {props.name}</h2>
     </div>
   );
 };
