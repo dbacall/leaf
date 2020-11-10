c871989b585d6fa9d584b32e75b7c768c64ff01f
dbacall <dbacall@hotmail.co.uk>
2020-11-10 15:59:01 +0000

    feat: add buttons and input for creating sunday league

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,11 +1,29 @@
 import React, { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import './styles/home.css';
 
 const Home = (props) => {
+  const [createLeague, setCreateLeague] = useState(false);
+  const [leagueName, setLeagueName] = useState('');
+
   return (
     <div>
       <h1>Home</h1>
       <h2>Hello {props.name}</h2>
+      <button onClick={() => setCreateLeague(!createLeague)}>
+        Create Sunday League
+      </button>
+      {createLeague ? (
+        <div>
+          <input
+            className="home-input"
+            type="text"
+            placeholder="Your league name here..."
+            value={leagueName}
+            onChange={(e) => setLeagueName(e.target.value)}
+          />
+          <button>Create</button>
+        </div>
+      ) : null}
     </div>
   );
 };
