4c2e0f4304b362ac124e240b6439b3cad6cf3d21
dbacall <dbacall@hotmail.co.uk>
2020-11-13 13:53:21 +0000

    feat: add button for adding teams

--- client/src/components/sundayLeague.js
+++ client/src/components/sundayLeague.js
@@ -1,9 +1,28 @@
 import React, { useState, useEffect } from 'react';
+import './styles/sunday-league.css';
 
 const SundayLeague = ({ league }) => {
+  const [addTeam, setAddTeam] = useState(false);
+  const [teamName, setTeamName] = useState('');
+
   return (
     <div>
       <h1>{league.leagueName}</h1>
+      <button onClick={() => setAddTeam(!addTeam)}>Add Team</button>
+      {addTeam ? (
+        <div>
+          <form>
+            <input
+              className="add-team-input"
+              type="text"
+              placeholder="Team name here..."
+              value={teamName}
+              onChange={(e) => setTeamName(e.target.value)}
+            />
+            <button>Add</button>
+          </form>
+        </div>
+      ) : null}
     </div>
   );
 };
