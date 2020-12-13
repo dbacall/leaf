20645762938c0a968f950b095ac3db2423118117
dbacall <dbacall@hotmail.co.uk>
2020-12-13 11:50:00 +0000

    remove commented out lines

--- server/test/test_helper.js
+++ server/test/test_helper.js
@@ -3,11 +3,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 beforeEach(async () => {
-  await mongoose.connection.dropDatabase(() => {});
-
-  // await mongoose.connection.collections.users.drop(() => {});
-  // await mongoose.connection.collections.sundayleagues.drop(() => {});
-  // await mongoose.connection.collections.sundayleagueteams.drop(() => {});
+  await mongoose.connection.dropDatabase(() => { });
 });
 
 // afterEach((done) => {
