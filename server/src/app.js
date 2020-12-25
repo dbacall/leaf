12a43297bae337101b420126525c47e2a500d061
dbacall <dbacall@hotmail.co.uk>
2020-12-25 11:03:13 +0000

    abstract server logic into server loader

--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,9 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const passport = require('passport');
 const app = express();
-const users = require('./routes/users');
-const sundayLeagues = require('./routes/sundayLeagues');
-const sundayLeagueTeam = require('./routes/sundayLeagueTeam');
+const database = require('./loaders/database');
+const server = require('./loaders/server');
 
-const cors = require('cors');
-const database = require('./loaders/database')
-
-// load environment variables. 
+// load environment variables.
 
 const dotenv_config = {};
 
@@ -23,36 +17,10 @@ if (!dotenv) {
   throw new Error('No .env file found.');
 }
 
-// process.env.NODE_PATH = __dirname;
-
 // connect to database
 
 database();
 
-app.use(cors());
-
-app.use(
-  bodyParser.urlencoded({
-    extended: false,
-  })
-);
-
-app.use(bodyParser.json());
-
-
-
-// Passport middleware
-app.use(passport.initialize());
-// Passport config
-require('./config/passport')(passport);
-
-// Routes
-app.use('/users', users);
-app.use('/sunday-leagues', sundayLeagues);
-app.use('/sunday-leagues/team', sundayLeagueTeam);
-
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Server up and running on port ${port}!`));
+server(app);
 
 module.exports = app;
---
+++ server/src/loaders/server.js
@@ -0,0 +1,33 @@
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const users = require('../routes/users');
+const sundayLeagues = require('../routes/sundayLeagues');
+const sundayLeagueTeam = require('../routes/sundayLeagueTeam');
+
+const cors = require('cors');
+
+module.exports = (app) => {
+  app.use(cors());
+
+  app.use(
+    bodyParser.urlencoded({
+      extended: false,
+    })
+  );
+
+  app.use(bodyParser.json());
+
+  // Passport middleware
+  app.use(passport.initialize());
+  // Passport config
+  require('../config/passport')(passport);
+
+  // Routes
+  app.use('/users', users);
+  app.use('/sunday-leagues', sundayLeagues);
+  app.use('/sunday-leagues/team', sundayLeagueTeam);
+
+  const port = process.env.PORT || 5000;
+
+  app.listen(port, () => console.log(`Server up and running on port ${port}!`));
+};
