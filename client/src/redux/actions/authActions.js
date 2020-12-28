5a41c2d4ec7b71c1f6fede804a968c0564b66a37
dbacall <dbacall@hotmail.co.uk>
2020-12-28 12:40:37 +0000

    update to use new api service

--- client/src/redux/actions/authActions.js
+++ client/src/redux/actions/authActions.js
@@ -1,11 +1,13 @@
-import axios from 'axios';
+import api from '../../services/api';
 import setAuthToken from '../../utils/setAuthToken';
 import jwt_decode from 'jwt-decode';
 import { GET_ERRORS, SET_CURRENT_USER, USER_LOADING } from './types';
 // Register User
 export const registerUser = (userData, history) => (dispatch) => {
-  axios
-    .post(`http://localhost:5000/users/register`, userData)
+  const path = '/users/register';
+
+  api
+    .request('post', userData, path)
     .then((res) => history.push('/login')) // re-direct to login on successful register
     .catch((err) =>
       dispatch({
@@ -16,25 +18,21 @@ export const registerUser = (userData, history) => (dispatch) => {
 };
 // Login - get user token
 export const loginUser = (userData) => (dispatch) => {
-  axios
-    .post(`http://localhost:5000/users/login`, userData)
+  const path = '/users/login';
+  api
+    .request('post', userData, path)
     .then((res) => {
-      // Save to localStorage
-      // Set token to localStorage
       const { token } = res.data;
 
       localStorage.setItem('jwtToken', token);
       // Set token to Auth header
       setAuthToken(token);
-      // Decode token to get user data
+
       const decoded = jwt_decode(token);
 
-      // Set current user
       dispatch(setCurrentUser(decoded));
     })
     .catch((err) =>
-      // console.log(res);
-
       dispatch({
         type: GET_ERRORS,
         payload: err.response.data,
