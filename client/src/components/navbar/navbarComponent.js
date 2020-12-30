c66fbe5d0fa26d4da1d5982a725c6a2972e2f134
dbacall <dbacall@hotmail.co.uk>
2020-12-30 14:47:58 +0000

    add styling for navbar and responsive mobile menu

--- client/src/components/navbar/navbarComponent.js
+++ client/src/components/navbar/navbarComponent.js
@@ -1,44 +1,59 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { logoutUser } from '../../redux/actions/authActions';
 import styles from './navbar-styles.module.scss';
+import { ReactComponent as CloseMenu } from '../../assets/x-mark.svg';
+import { ReactComponent as MenuIcon } from '../../assets/menu.svg';
 
-class Navbar extends Component {
-  onLogoutClick = (e) => {
-    e.preventDefault();
-    this.props.logoutUser();
+const Navbar = ({ user, logout }) => {
+  const [mobileMenu, setMobileMenu] = useState(false);
+
+  const menuActive = mobileMenu ? styles.active : '';
+
+  const handleMenu = () => {
+    setMobileMenu(!mobileMenu);
   };
 
-  render() {
-    const { user } = this.props.auth;
-    return (
-      <section className={styles.navbar}>
-        <nav>
-          <Link to="/" className={styles.title}>
-            <span>Fantasy Me</span>
-          </Link>
-          {user ? (
-            <div className={styles.navButtons}>
-              <Link to="/my-sunday-leagues" className={styles.navButton}>
-                My Sunday Leagues
-              </Link>
-              <Link to="/" className={styles.navButton}>
-                My Fantasy Leagues
-              </Link>
-              <a onClick={this.onLogoutClick} className={styles.navButton}>
-                Logout
-              </a>
-            </div>
-          ) : null}
-        </nav>
-      </section>
-    );
-  }
-}
+  const onLogoutClick = (e) => {
+    e.preventDefault();
+    logout();
+    handleMenu();
+  };
 
-const mapStateToProps = (state) => ({
-  auth: state.auth,
-});
+  return (
+    <section className={styles.navbar}>
+      <nav>
+        <Link to="/" className={styles.title} onClick={handleMenu}>
+          <span>Fantasy Me</span>
+        </Link>
+        {user ? (
+          <div className={`${styles.navButtons} ${menuActive}`}>
+            <Link
+              to="/my-sunday-leagues"
+              className={styles.navButton}
+              onClick={handleMenu}
+            >
+              My Sunday Leagues
+            </Link>
+            <Link to="/" className={styles.navButton} onClick={handleMenu}>
+              My Fantasy Leagues
+            </Link>
+            <a className={styles.navButton} onClick={onLogoutClick}>
+              Logout
+            </a>
+          </div>
+        ) : null}
+        {user ? (
+          <div className={styles.mobileMenuIcon} onClick={handleMenu}>
+            {mobileMenu ? (
+              <CloseMenu className={styles.menuIcon} />
+            ) : (
+              <MenuIcon className={styles.menuIcon} />
+            )}
+          </div>
+        ) : null}
+      </nav>
+    </section>
+  );
+};
 
-export default connect(mapStateToProps, { logoutUser })(Navbar);
+export default Navbar;
--- client/src/components/navbar/navbarContainer.js
+++ client/src/components/navbar/navbarContainer.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import Navbar from './navbarComponent';
+import { useSelector, useDispatch } from 'react-redux';
+import { logoutUser } from '../../redux/actions/authActions';
 
 const NavbarContainer = () => {
-  return <Navbar />;
+  const user = useSelector((state) => state.auth.user);
+
+  const dispatch = useDispatch();
+
+  const logout = () => {
+    dispatch(logoutUser());
+  };
+
+  return <Navbar user={user} logout={logout} />;
 };
 
 export default NavbarContainer;
