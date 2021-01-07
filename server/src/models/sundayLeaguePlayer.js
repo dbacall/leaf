2d863be13f2ceeb81f189b3b31eb7642bf3d100a
dbacall <dbacall@hotmail.co.uk>
2021-01-07 11:27:14 +0000

    add test for player name being first and last name

--- server/src/models/sundayLeaguePlayer.js
+++ server/src/models/sundayLeaguePlayer.js
@@ -2,7 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const sundayLeaguePlayerSchema = new Schema({
-  name: String,
+  name: {
+    type: String,
+    match: [
+      /[a-zA-Z]+\s+[a-zA-Z]+/g,
+      'Player name must be a first and last name',
+    ],
+  },
   position: String,
   team: {
     type: Schema.Types.ObjectId,
--- server/src/test/spec/sundayLeaguePlayer.test.js
+++ server/src/test/spec/sundayLeaguePlayer.test.js
@@ -2,7 +2,7 @@ const expect = require('chai').expect;
 const SundayLeaguePlayer = require('../../models/sundayLeaguePlayer');
 
 describe('Sunday league player tests:', () => {
-  it.only('should let a user add a player to a team', async () => {
+  it('should let a user add a player to a team', async () => {
     const user = await registerUser(
       'David',
       'Bacall',
@@ -31,5 +31,32 @@ describe('Sunday league player tests:', () => {
     expect(result.name).to.equal('Bob Smith');
     expect(result.position).to.equal('Defender');
     expect(result.team.toString()).to.eq(teamAdded.body.data.id);
+  });
+
+  it.only('should throw an error if player name is only one word', async () => {
+    const user = await registerUser(
+      'David',
+      'Bacall',
+      'dbacall@hotmail.co.uk',
+      'password',
+      'password'
+    );
+
+    const leagueAdded = await addSundayLeague('league1', user._id);
+
+    const teamAdded = await addSundayLeagueTeam(
+      'team1',
+      leagueAdded.body.data.id
+    );
+
+    const result = await addSundayLeaguePlayer(
+      'Bob',
+      'Defender',
+      teamAdded.body.data.id
+    );
+
+    expect(result.body.errors.name.message).to.equal(
+      'Player name must be a first and last name'
+    );
   });
 });
