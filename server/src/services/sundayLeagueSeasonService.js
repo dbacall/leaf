727bf2ea2f7947d10380675f5bcaa56b94b9fef1
dbacall <dbacall@hotmail.co.uk>
2021-01-16 11:14:25 +0000

    abstract getIndividualResults into utilities folder

--- server/src/services/sundayLeagueSeasonService.js
+++ server/src/services/sundayLeagueSeasonService.js
@@ -1,5 +1,6 @@
 const Service = require('./service');
 const SundayLeagueFixture = require('../models/sundayLeagueFixture')
+const getIndividualResults = require('./utilities/getIndividualResults')
 
 class SundayLeagueSeasonService extends Service {
   constructor(model) {
@@ -25,36 +26,9 @@ class SundayLeagueSeasonService extends Service {
       let seasonResults = []
       for (const team of teams) {
         const fixtures = await SundayLeagueFixture.find({ $or: [{ homeTeam: team.id }, { awayTeam: team.id }] })
-        let teamResults = {
-          name: team.name,
-          id: team.id,
-          won: 0,
-          lost: 0,
-          drawn: 0,
-          goalsFor: 0,
-          goalsAgainst: 0,
-          points: 0,
-        }
-        teamResults.name = team.name
-        teamResults.id = team.id
-        for (const fixture of fixtures) {
-          if (fixture.winner == team.id) {
-            teamResults.won += 1
-            teamResults.points += 3
-          } else if (fixture.draw) {
-            teamResults.drawn += 1
-            teamResults.points += 1
-          } else teamResults.lost += 1
 
-          if (fixture.homeTeam == team.id) {
-            teamResults.goalsFor += fixture.homeTeamGoals
-            teamResults.goalsAgainst += fixture.awayTeamGoals
-          }
-          if (fixture.awayTeam == team.id) {
-            teamResults.goalsFor += fixture.awayTeamGoals
-            teamResults.goalsAgainst += fixture.homeTeamGoals
-          }
-        }
+        const teamResults = getIndividualResults(team, fixtures)
+
         seasonResults.push(teamResults)
       }
       return {
@@ -70,6 +44,7 @@ class SundayLeagueSeasonService extends Service {
       };
     }
   }
+
 }
 
 module.exports = SundayLeagueSeasonService;
