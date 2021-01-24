a0dcc965131388c57a3a2936f71124c0b881f33e
dbacall <dbacall@hotmail.co.uk>
2021-01-24 22:08:17 +0000

    rename modal folder and file

--- client/src/components/commons/Modal/modal.js
+++ client/src/components/commons/Modal/modal.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styles from './modal.module.scss'
 import { ReactComponent as CloseModal } from '../../../assets/icons/x-mark.svg';
-import SundayFixture from '../../sundayFixture/sundayFixtureContainer'
 
 const Modal = ({ children, open, onClose }) => {
   return (
