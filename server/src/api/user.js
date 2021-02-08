8cedcccb09688d386175cbdd54ac39f58c9fed6d
dbacall <dbacall@hotmail.co.uk>
2021-02-08 14:35:20 +0000

    add route for updating user

--- server/src/api/user.js
+++ server/src/api/user.js
@@ -4,5 +4,6 @@ const UserController = require('../controllers/userController');
 
 router.post('/register', UserController.register);
 router.post('/login', UserController.login);
+router.put('/:id', UserController.update)
 
 module.exports = router;
