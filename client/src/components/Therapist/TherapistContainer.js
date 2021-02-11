3feba1d4b0e2fe7ce2aa6f9670b60219414861ef
dbacall <dbacall@hotmail.co.uk>
2021-02-11 16:02:47 +0000

    add meeting table styling

--- client/src/components/Therapist/TherapistContainer.js
+++ client/src/components/Therapist/TherapistContainer.js
@@ -2,30 +2,35 @@ import React, { useEffect, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Therapist from './TherapistComponent';
 import { fetchTherapist } from '../../redux/thunks/TherapistsThunks';
-import { useParams } from 'react-router-dom'
-
+import { useParams } from 'react-router-dom';
 
 const TherapistContainer = () => {
   const dispatch = useDispatch();
 
   const user = useSelector((state) => state.auth.user);
 
-  const { status, selectedTherapist } = useSelector((state) => state.therapists)
-  const { id, category } = useParams()
+  const { status, selectedTherapist } = useSelector(
+    (state) => state.therapists
+  );
+  const { id, category } = useParams();
 
   const isInitialMount = useRef(true);
 
   useEffect(() => {
-    console.log('here');
-    if (
-      isInitialMount.current
-    ) {
+    if (isInitialMount.current) {
       isInitialMount.current = false;
       dispatch(fetchTherapist(id));
     }
   }, []);
 
-  return <Therapist user={user} therapist={selectedTherapist} status={status} category={category} />;
+  return (
+    <Therapist
+      user={user}
+      therapist={selectedTherapist}
+      status={status}
+      category={category}
+    />
+  );
 };
 
 export default TherapistContainer;
