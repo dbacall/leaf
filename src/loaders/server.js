e0a954016a78205ed49ec95edf351892eb4e296a
dbacall <dbacall@hotmail.co.uk>
2021-02-17 07:31:44 +0000

    add path.resolve to server

--- src/loaders/server.js
+++ src/loaders/server.js
@@ -9,9 +9,9 @@ module.exports = (app) => {
   // serve the react app
   app.use(express.static(path.join(__dirname, '../../client/build')));
 
-  // app.get('*', (req, res) => {
-  //   res.sendFile(path.join(__dirname, '/client/public/index.html'));
-  // });
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '/client/public/index.html'));
+  });
 
   app.use(cors());
 
