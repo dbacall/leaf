d35d646a7e0f8933cebaaccdd62687a96537135a
dbacall <dbacall@hotmail.co.uk>
2021-02-17 01:09:42 +0000

    update build

---
+++ client/build/precache-manifest.5f27fd06fb8e754cbecbff051fdd2502.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88f9bca7b090f02f134716ed744fe78e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b40d20dce35f297b7b9f",
+    "url": "/static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "fd1d2bb3be46b80382af",
+    "url": "/static/css/main.c2a439fa.chunk.css"
+  },
+  {
+    "revision": "b40d20dce35f297b7b9f",
+    "url": "/static/js/2.f949acbc.chunk.js"
+  },
+  {
+    "revision": "da84f90284f5fadf549b80f80d3ab0b2",
+    "url": "/static/js/2.f949acbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd1d2bb3be46b80382af",
+    "url": "/static/js/main.31b26a15.chunk.js"
+  },
+  {
+    "revision": "200d81c13bc454795583",
+    "url": "/static/js/runtime-main.a43146e1.js"
+  },
+  {
+    "revision": "8945ca0c7c4d34bd1c76107d81352405",
+    "url": "/static/media/Mums-walking.8945ca0c.jpg"
+  },
+  {
+    "revision": "8b8449c504fe92f617d4bd3ec7c4a966",
+    "url": "/static/media/cheese-selection-p313-443_medium.8b8449c5.jpg"
+  },
+  {
+    "revision": "00b27c6bbcdc5e3c924b155ef99e5b23",
+    "url": "/static/media/menu.00b27c6b.svg"
+  },
+  {
+    "revision": "62eefd11215424ec653ab194330af840",
+    "url": "/static/media/x-mark.62eefd11.svg"
+  }
+]);
