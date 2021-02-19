45607632820fca4b1a34a71dfa064dcbabbfda76
dbacall <dbacall@hotmail.co.uk>
2021-02-19 07:48:58 +0000

    remove client from leaf repo

--- client/src/components/commons/Loader.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { SolarSystemLoading } from 'react-loadingg'
-
-const Loader = () => {
-  return (<SolarSystemLoading />);
-}
-
-export default Loader;
--- client/src/config/config.js
+++
@@ -1,9 +0,0 @@
-const prod = {
-  url: {
-    API_URL: 'https://therapy-box-tech-test.herokuapp.com',
-  },
-};
-
-const devUrl = 'http://localhost:5000';
-
-export const config = process.env.NODE_ENV === 'development' ? devUrl : prod;
